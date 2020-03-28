351346dcf4fe1c36aad49f5c92dd71f1dd4371fc
Michael Atkinson <m.g.atkinson@outlook.com>
2020-03-28 13:55:37 -0400

    add ReceiveMessage capabilities

--- lambda/donation_processor/donation_processor.py
+++ lambda/donation_processor/donation_processor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import boto3
-# import os
+import os
 
 
 # Function for executing athena queries
@@ -20,5 +20,19 @@ import boto3
 
 
 def lambda_handler(event, context):
-
+    sqs_client = boto3.client('sqs')
+    response = sqs_client.receive_message(
+        QueueUrl=os.environ['QueueUrl'],
+        # AttributeNames=[
+        #     'All'|'Policy'|'VisibilityTimeout'|'MaximumMessageSize'|'MessageRetentionPeriod'|'ApproximateNumberOfMessages'|'ApproximateNumberOfMessagesNotVisible'|'CreatedTimestamp'|'LastModifiedTimestamp'|'QueueArn'|'ApproximateNumberOfMessagesDelayed'|'DelaySeconds'|'ReceiveMessageWaitTimeSeconds'|'RedrivePolicy'|'FifoQueue'|'ContentBasedDeduplication'|'KmsMasterKeyId'|'KmsDataKeyReusePeriodSeconds',
+        # ],
+        # MessageAttributeNames=[
+        #     'string',
+        # ],
+        MaxNumberOfMessages=1,
+        # VisibilityTimeout=123,
+        # WaitTimeSeconds=123,
+        # ReceiveRequestAttemptId='string'
+    )
+    print(response)
     return 'Success!'
