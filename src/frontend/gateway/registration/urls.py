b8dab120f12200cd261ad7f80e3b6622c61fa879
Bobby Steinbach <steinbach.rj@gmail.com>
2020-03-28 01:17:08 -0400

    adds about page content

--- src/frontend/gateway/registration/urls.py
+++ src/frontend/gateway/registration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from registration import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
