d0101663ad479e56694331b9dbaf3eed627c4eab
Bobby Steinbach <steinbach.rj@gmail.com>
2020-03-30 15:36:24 -0400

    fixes lookup_instance_cost lambda to handle case when ActualBlockHourlyPrice isnt set

--- lambda/lookup_instance_cost/lookup_instance_cost.py
+++ lambda/lookup_instance_cost/lookup_instance_cost.py
@@ -36,7 +36,13 @@ def lambda_handler(event, context):
                 },
             ]
         )
-        hourly_price_str = response['SpotInstanceRequests'][0]['ActualBlockHourlyPrice']
+
+        spot_data = response['SpotInstanceRequests'][0]
+        try:
+            hourly_price_str = spot_data['ActualBlockHourlyPrice']
+        except KeyError:
+            hourly_price_str = spot_data['SpotPrice']
+            
         hourly_price_decimal = Decimal(hourly_price_str.strip(' "'))
         hourly_price_cents = hourly_price_decimal * 100
         return str(-1 * hourly_price_cents/60 * scheduler_frequency_in_minutes)
